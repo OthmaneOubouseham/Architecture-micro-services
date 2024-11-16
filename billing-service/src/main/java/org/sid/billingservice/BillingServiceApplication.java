ce13cb050ccff27054df5abca8f2cac5d3b5ca80
OthmaneOubouseham <othmaneoubouselham611@gmail.com>
2024-11-16 12:36:43 +0100

    version 2

--- billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/org/sid/billingservice/BillingServiceApplication.java
@@ -1,13 +1,29 @@
 package org.sid.billingservice;
 
+import org.sid.billingservice.feign.CustomerRestClient;
+import org.sid.billingservice.feign.ProductRestClient;
+import org.sid.billingservice.repository.BillRepository;
+import org.sid.billingservice.repository.ProductItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BillingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(BillRepository billRepository,
+                                        ProductItemRepository productItemRepository,
+                                        CustomerRestClient customerRestClient,
+                                        ProductRestClient productRestClient) {
+        
+
     }
 
 }
